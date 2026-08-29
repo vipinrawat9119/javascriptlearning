e27ccf66bebb233d3da3aa9b993891dab467dea5
Vipin Rawat <vipinrawat9119@gmail.com>
2026-08-29 12:52:23 +0530

    Added JavaScript test.js practice file

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,58 @@
-console.log("Vipin")
+ console.log("Vipin245")
+
+// // About VAriables 
+// const accountId = 144553
+// let accountEmail = "hitesh@google.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState;
+
+// // accountId = 2 // not allowed
+
+
+// accountEmail = "hc@hc.com"
+// accountPassword = "21212121"
+// accountCity = "Bengaluru"
+
+// console.log(accountId);
+
+// /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// // DAta types 
+
+// "use strict"; // treat all JS code as newer version
+
+// // alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Hitesh")
+
+
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+
+// // number => 2 to power 53
+// // bigint
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undefined => 
+// // symbol => unique
+
+
+// // object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
